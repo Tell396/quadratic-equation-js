8b060954925a4cda84d2cde1c35378b43503dd15
Cenhelm Aston <vova21473@gmail.com>
2022-01-22 15:13:55 +1000

    Story #3. Added Web UI.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
-console.clear();
-let a = prompt('Enter A factorial');
-let b = prompt('Enter B factorial');
-let c = prompt('Enter C factorial');
 
-console.log(solution(a, b, c));
-
-
-function solution(a, b, c) {
+function answer() {
+  console.clear()
+  
+  let a = document.querySelector("#first-coef").value;
+  let b = document.querySelector("#second-coef").value;
+  let c = document.querySelector("#third-coef").value;
+  
   console.log(`a = ${a}; \nb = ${b}; \nc = ${c};`);
 
   let D = Math.pow(b, 2) - 4 * a * c;
   console.log(`D = b² - 4ac = ${Math.pow(a, 2)} - 4 * ${b} * ${c} = ${D}`);
+  
+  let formula = document.querySelector('#formula')
 
   if (D < 0) {
-    console.log(`${D} < 0. This quadratic equation has no roots`);
+    formula.innerHTML = `
+      <p>${D} < 0.<br>This quadratic equation has no roots</p>`
   } else if (D > 0) {
-    console.log("x1,2 = -b ± √D / 2a");
     let plus = (-b + Math.sqrt(D)) / (2 * a);
     let minus = (-b - Math.sqrt(D)) / (2 * a);
-    console.log(`x1 = -b + √D / 2a = ${plus} \nx2 = -b - √D / 2a ${minus}`);
+    
+    formula.innerHTML = `
+      <p>x1,2 = -b ± √D / 2a</p>
+      <p>x1 = -b + √D / 2a = ${plus} <br> x2 = -b - √D / 2a ${minus}</p>`
   } else if (D === 0) {
-    console.log((-b + Math.sqrt(D)) / (2 * a));
+    formula.innerHTML = `
+      <p>${(-b + Math.sqrt(D)) / (2 * a)}</p>`
   }
-}
+  
+  formula.style.backgroundColor = "#0001"
+  formula.style.borderRadius = "7px"
+  formula.style.padding = "10px 5px 10px 5px"
+};
 
