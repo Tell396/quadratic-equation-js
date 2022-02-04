27f0f1d4cb92854b7b825afdd38d3eacc74c8b19
Cenhelm Aston <55622895+Tell396@users.noreply.github.com>
2022-02-04 14:26:23 +1000

    Story #8. Fixed some bugs

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@
 function answer() {
   console.clear();
 
-  let a = document.querySelector('#first-coef').value;
-  let b = document.querySelector('#second-coef').value;
-  let c = document.querySelector('#third-coef').value;
+  let a = document.querySelector("#first-coef").value;
+  let b = document.querySelector("#second-coef").value;
+  let c = document.querySelector("#third-coef").value;
 
-  let formula = document.querySelector('#formula');
-  let vieta = document.querySelector('#vieta');
+  let formula = document.querySelector("#formula");
+  let vieta = document.querySelector("#vieta");
 
   let D = Math.pow(b, 2) - 4 * a * c;
 
@@ -19,17 +19,22 @@ function answer() {
   let x1 = (-b + Math.sqrt(D)) / (2 * a);
   let x2 = (-b - Math.sqrt(D)) / (2 * a);
 
-  if (D < 0) {
-    formula.innerHTML = `
-      <p>a = ${a}; <br>b = ${b}; <br>c = ${c};</p>
-      <p>D = b² - 4ac = ${Math.pow(a, 2)} - 4 * ${b} * ${c} = ${D};</p>
-      <p>${D} < 0.<br>This quadratic equation has no roots.</p>
-      `;
-  } else if (D > 0) {
+  if (D > 0) {
     formula.innerHTML = `
       <p>D = b² - 4ac = ${Math.pow(a, 2)} - 4 * ${b} * ${c} = ${D}</p>
       <p>x1,2 = -b ± √D / 2a</p>
       <p>x1 = -b + √D / 2a = ${x1} <br> x2 = -b - √D / 2a ${x2}</p>
+      `;
+    vieta.innerHTML = `
+      <h3>Vieta's formulas for the quartic:</h3>
+      <p>x1 + x2 = ${-Math.round(b)} <br>
+      x1 * x2 = ${c}</p>
+      `;
+  } else if (D < 0) {
+    formula.innerHTML = `
+      <p>a = ${a}; <br>b = ${b}; <br>c = ${c};</p>
+      <p>D = b² - 4ac = ${Math.pow(a, 2)} - 4 * ${b} * ${c} = ${D};</p>
+      <p>${D} < 0.<br>This quadratic equation has no roots.</p>
       `;
   } else if (D === 0) {
     if ((-b + Math.sqrt(D)) / (2 * a) == NaN) {
@@ -37,15 +42,13 @@ function answer() {
           <p>a = ${a}; <br>b = ${b}; <br>c = ${c};</p>
           ${(-b + Math.sqrt(D)) / (2 * a) == NaN}`;
     } else {
-      formula.innerHTML = '<p>This quadratic equation has no roots.</p>';
+      formula.innerHTML = `
+      <p>D = b² - 4ac = ${Math.pow(a, 2)} - 4 * ${b} * ${c} = ${D}</p>
+      <p>x1,2 = -b ± √D / 2a</p>
+      <p>x = -b + √D / 2a = ${x1}`;
     }
   }
 
-  formula.style.borderRadius = '7px';
-  formula.style.padding = '10px 5px 10px 5px';
-  vieta.innerHTML = `
-  <h3>Vieta's formulas for the quartic:</h3>
-  <p>x1 + x2 = ${-Math.round(b)} <br>
-  x1 * x2 = ${c}</p>
-  `;
+  formula.style.borderRadius = "7px";
+  formula.style.padding = "10px 5px 10px 5px";
 }
